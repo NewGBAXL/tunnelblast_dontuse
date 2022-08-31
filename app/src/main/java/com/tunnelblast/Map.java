aaa07d56eb09dab9d319544f88f8e4cdb71f19b0
NewGBAXL <81663474+NewGBAXL@users.noreply.github.com>
2022-08-31 12:12:46 -0400

    cell object created, implemented fully in map.java

--- app/src/main/java/com/tunnelblast/Map.java
+++ app/src/main/java/com/tunnelblast/Map.java
@@ -23,124 +23,133 @@ import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class Map extends View
-{
-    int rows = 15;
-    int cols = 15;
-    public int[][] array = new int[rows][cols];
+public class Map extends View {
+    int xWidth = 15; //14
+    int yHeight = 15; //7
     private float cellSize, hMargin, vMargin;
     private Paint wallPaint;
     private Paint wallPaintGhost;
     private static final float WALL_THICKNESS = 4;
+    private Cell[][] cells;
 
-    public Map (Context context, @Nullable AttributeSet attrs){ super (context, attrs);
+    public Map(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
         wallPaint = new Paint();
         wallPaint.setColor(Color.BLACK);
         wallPaint.setStrokeWidth(WALL_THICKNESS);
         wallPaintGhost = new Paint();
         wallPaintGhost.setColor(Color.BLUE);
         wallPaintGhost.setStrokeWidth(WALL_THICKNESS);
+        createMaze();
+    }
 
-        //rows = nRows * 2 + 1;
-        //cols = nCols + 1;
-        array = new int[rows][cols];
-
-        //sets first & last rows to edge property
-        for (int i = 0; i < cols; ++i)
-        {
-            array[0][i] = -1;
-            array[rows - 1][i] = -1;
+    private void createMaze() {
+        for (int x = 0; x < xWidth; ++x) {
+            for (int y = 0; y < yHeight; ++y) {
+                cells[x][y] = new Cell(x, y);
+            }
         }
-        //sets first & last cols to edge property
-        for (int i = 0; i < rows; ++i) {
-            array[i][0] = -1;
-            if (i % 2 == 0) {
-                array[i][cols - 2] = -1;
-                array[i][cols - 1] = -2;
-            } else
-                array[i][0] = -1;
 
-
+        for (int x = 0; x < xWidth; ++x) {
+            cells[x][0].buildWall((byte)0, -1); //sets first nWall to -1
+            cells[x][yHeight - 1].buildWall((byte)2, -1); //sets last sWall to -1
+        }
+        for (int y = 0; y < yHeight; ++y) {
+            cells[0][y].buildWall((byte)3, -1); //sets first wWall to -1
+            cells[xWidth - 1][y].buildWall((byte)1, -1); //sets last eWall to -1
         }
     }
 
     @Override
-    protected void onDraw(Canvas canvas){
+    protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.GREEN);
         int width = getWidth();
-        int height  = getHeight();
+        int height = getHeight();
 
-        if (width / height < cols / rows)
-            cellSize = width / (cols + 1);
+        if (width / height < xWidth / yHeight)
+            cellSize = width / (xWidth + 1);
         else
-            cellSize = height / (rows + 1);
+            cellSize = height / (yHeight + 1);
 
-        hMargin = (width - cols * cellSize)/2;
-        vMargin = (height - rows * cellSize)/2;
+        hMargin = (width - xWidth * cellSize) / 2;
+        vMargin = (height - yHeight * cellSize) / 2;
         canvas.translate(hMargin, vMargin);
 
-        for (int i = 0; i < cols; ++i){
-            for (int j = 0; j < rows; ++j){
-                //if (array[i][j] != 0 && i % 2 == 0) //if is top wall
+        //north walls
+        for (int i = 0; i < xWidth; ++i) {
+            for (int j = 0; j < yHeight; ++j) {
+                canvas.drawLine( //if -1 or >0 wall, if 0 show ghost
+                        i * cellSize, j * cellSize, (i + 1) * cellSize,
+                        j * cellSize, (cells[i][j].nWall != 0) ? wallPaint : wallPaintGhost);
+            }
+        }
+        //south walls
+        for (int i = 0; i < xWidth; ++i) {
+            for (int j = 0; j < yHeight; ++j) {
                 canvas.drawLine(
-                        i*cellSize, j*cellSize, (i+1)* cellSize,
-                        j*cellSize, (array[i][j]==-1)?wallPaint:wallPaintGhost);
-                //if (array[i][j] != 0 && i % 2 == 1)
-                    canvas.drawLine(
-                            i*cellSize, j*cellSize, i* cellSize,
-                            (j+1)*cellSize, (array[i][j]==-1)?wallPaint:wallPaintGhost);
-            }//if -2 dont show, if -1 or >0 wall, if 0 show ghost
+                        i * cellSize, (j+1) * cellSize, (i + 1) * cellSize,
+                        (j+1) * cellSize, (cells[i][j].sWall != 0) ? wallPaint : wallPaintGhost);
+            }
+        }
+        //east walls
+        for (int i = 0; i < xWidth; ++i) {
+            for (int j = 0; j < yHeight; ++j) {
+                canvas.drawLine(
+                        (i+1) * cellSize, j * cellSize, (i+1) * cellSize,
+                        (j + 1) * cellSize, (cells[i][j].eWall != 0) ? wallPaint : wallPaintGhost);
+            }
+        }
+        //west walls
+        for (int i = 0; i < xWidth; ++i) {
+            for (int j = 0; j < yHeight; ++j) {
+                canvas.drawLine(
+                        i * cellSize, j * cellSize, i * cellSize,
+                        (j + 1) * cellSize, (cells[i][j].wWall != 0) ? wallPaint : wallPaintGhost);
+            }
         }
     }
 
-    boolean buildWall(int x, int y) {
-        if (array[x][y] == 0) //if no wall exists
-        {
-            array[x][y] = 1;
-            return true;
+    void breakWall(int x, int y, byte wallId, int str) {
+        //if cell method return true, destroy adj wall on adj cell
+        if (cells[x][y].breakWall(wallId, str)) {
+            if (wallId == 0)
+                cells[x][y - 1].breakWall((byte)2, str);
+            if (wallId == 1)
+                cells[x + 1][y].breakWall((byte)3, str);
+            if (wallId == 2)
+                cells[x][y + 1].breakWall((byte)0, str);
+            if (wallId == 3)
+                cells[x - 1][y].breakWall((byte)1, str);
         }
-        return false;
     }
 
-    boolean buildWall(int x, int y, int strength)
-    {
-        if (array[x][y] == 0) //if no wall exists
-        {
-            array[x][y] = strength;
-            return true;
+    void buildWall(int x, int y, byte wallId, int str) {
+        //if cell method return true, build adj wall on adj cell
+        if (cells[x][y].buildWall(wallId, str)) {
+            if (wallId == 0)
+                cells[x][y - 1].buildWall((byte)2, str);
+            if (wallId == 1)
+                cells[x + 1][y].buildWall((byte)3, str);
+            if (wallId == 2)
+                cells[x][y + 1].buildWall((byte)0, str);
+            if (wallId == 3)
+                cells[x - 1][y].buildWall((byte)1, str);
         }
-        return false;
     }
 
-    void breakWall(int x, int y)
-    {
-        if (array[x][y] > 0)
-            --array[x][y];
-    }
+    //addCar method
+    //eraseMap method //planned feature
+    //randomGenerate method //planned feature
 
-    //if edge, set to -1
-    //if no wall, set to 0
-    //if wall has strength n, set to n
-    //formatting set to -2
-
-    //-----
-    //||||||
-    //-----
-    //||||||
-    //-----
-
-    //following nums are all negative
-    //sample 5x5 array
-    //111112
-    //100001 row 1
-    //100012
-    //100001 row 2
-    //100012
-    //100001 row 3
-    //100012
-    //100001 row 4
-    //100012
-    //100001 row 5
-    //111112
-
+    //methods in other classes
+    //winUI method
+    //loseUI method
+    //carBlowUp method
+    //carGun method
+    //moveTo (car) method (where?)
 }
+
+//if edge, set to -1
+//if no wall, set to 0
+//if wall has strength n, set to n
+//if exit set to -2
